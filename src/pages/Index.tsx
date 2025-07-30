8c0ca0981bd9a7b4f3117a82e201f839cfa70950
[dyad] <vqtrung357@gmail.com>
2025-07-30 15:23:50 +0700

    [dyad] Restore missing page sections - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,34 @@
+import BrandNarrative from "../components/landing/BrandNarrative";
 import BrandPosition from "../components/landing/BrandPosition";
+import BrandValues from "../components/landing/BrandValues";
+import BrandVision from "../components/landing/BrandVision";
+import CTA from "../components/landing/CTA";
+import FAQ from "../components/landing/FAQ";
 import Footer from "../components/landing/Footer";
+import Header from "../components/landing/Header";
+import Hero from "../components/landing/Hero";
+import Portfolio from "../components/landing/Portfolio";
 import ShowcaseImage from "../components/landing/ShowcaseImage";
+import Testimonials from "../components/landing/Testimonials";
 
 const IndexPage = () => {
   return (
-    <main className="bg-black text-white">
-      <BrandPosition />
-      <ShowcaseImage />
+    <div className="bg-black text-white font-sans">
+      <Header />
+      <main>
+        <Hero />
+        <BrandPosition />
+        <ShowcaseImage />
+        <BrandNarrative />
+        <BrandVision />
+        <BrandValues />
+        <Portfolio />
+        <Testimonials />
+        <FAQ />
+        <CTA />
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 };
 
