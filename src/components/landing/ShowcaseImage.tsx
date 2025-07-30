99be586cfae0a0b9f8651d7a7a55b6f5e14f9394
[dyad] <vqtrung357@gmail.com>
2025-07-30 15:22:26 +0700

    [dyad] Update showcase image - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/landing/ShowcaseImage.tsx
+++ src/components/landing/ShowcaseImage.tsx
@@ -3,7 +3,7 @@ const ShowcaseImage = () => {
     <section className="bg-black pb-20 md:pb-32">
       <div className="container mx-auto px-4 md:px-6">
         <img
-          src="/placeholder.png" // <-- Let me know the real name of your image file!
+          src="/cyberk.png"
           alt="Showcase"
           className="rounded-lg shadow-2xl mx-auto w-full max-w-6xl"
         />
