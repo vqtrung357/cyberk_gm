a1e25106756d2bc932c2082567ac54134673322f
[dyad] <vqtrung357@gmail.com>
2025-07-30 15:07:22 +0700

    [dyad] Move Brand Position section down - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,29 @@
-import Header from "@/components/landing/Header";
-import Hero from "@/components/landing/Hero";
-import BrandNarrative from "@/components/landing/BrandNarrative";
-import BrandVision from "@/components/landing/BrandVision";
-import BrandValues from "@/components/landing/BrandValues";
-import BrandPosition from "@/components/landing/BrandPosition";
-import Portfolio from "@/components/landing/Portfolio";
-import Contact from "@/components/landing/Contact";
+import BrandNarrative from '@/components/landing/BrandNarrative';
+import BrandPosition from '@/components/landing/BrandPosition';
+import BrandValues from '@/components/landing/BrandValues';
+import BrandVision from '@/components/landing/BrandVision';
+import CTA from '@/components/landing/CTA';
+import FAQ from '@/components/landing/FAQ';
+import Footer from '@/components/landing/Footer';
+import Header from '@/components/landing/Header';
+import Hero from '@/components/landing/Hero';
+import Testimonials from '@/components/landing/Testimonials';
 
 const Index = () => {
   return (
-    <div className="bg-black text-white font-sans">
+    <div className="bg-black text-white">
       <Header />
       <main>
         <Hero />
         <BrandNarrative />
-        <BrandVision />
-        <BrandValues />
         <BrandPosition />
-        <Portfolio />
-        <Contact />
+        <BrandValues />
+        <BrandVision />
+        <Testimonials />
+        <FAQ />
+        <CTA />
       </main>
+      <Footer />
     </div>
   );
 };
